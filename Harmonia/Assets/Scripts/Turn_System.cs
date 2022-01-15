1066c5ec5b7068008bfa333e020e3038bfd919f7
Kevin Lewis <klewis5@ucsc.edu>
2022-01-14 18:36:11 -0800

    UI Changed for talking scene

---
+++ Harmonia/Assets/Scripts/Turn_System.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class turn_system : MonoBehaviour
+{
+    // vars
+    private BattleState state;
+    private GameObject player;
+    private GameObject enemy;
+
+    public Transform playerSpawn;
+    public Transform enemySpawn;
+
+    // player objects
+    // character playerChara
+    // character enemyChara
+
+    public static turn_system instance;
+
+    void Start()
+    {
+        instance = this;
+        state = BattleState.START;
+        StartCoroutine(SetupBattle());
+    }
+
+    IEnumerator SetupBattle()
+    {
+        // Setup and spawn characters and load whatever needs to be loaded
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
+    }
+
+    void PlayerTurn()
+    {
+        // enable player to make choices for turn
+    }
+
+    // when an option is picked
+    public void OnChoicePicked()
+    {
+        if (state != BattleState.PLAYERTURN)
+        {
+            return;
+        }
+
+        StartCoroutine(PlayerPerform());
+    }
+
+    IEnumerator PlayerPerform()
+    {
+        state = BattleState.ENEMYTURN;
+        // perform song
+        yield return new WaitForSeconds(2f);
+
+        // enemy take damage
+        // bool isDead = enemyChara.TakeDamage(damage);
+        bool isDead = false;
+        // update HUDs
+        if (isDead)
+        {
+            state = BattleState.WON;
+            EndBattle();
+        }
+        else
+        {
+            StartCoroutine(EnemyTurn());
+        }
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        state = BattleState.PLAYERTURN;
+        // enemy performs
+        yield return new WaitForSeconds(2f);
+
+        // player take damage
+        //  bool isDead = enemyChara.TakeDamage(damage);
+        bool isDead = false;
+
+        if (isDead)
+        {
+            state = BattleState.LOST;
+            EndBattle();
+        }
+        else
+        {
+            PlayerTurn();
+        }
+    }
+
+    void EndBattle()
+    {
+        if (state == BattleState.WON)
+        {
+            // win
+        }
+        else if (state == BattleState.LOST)
+        {
+            // lose
+        }
+    }
+}
