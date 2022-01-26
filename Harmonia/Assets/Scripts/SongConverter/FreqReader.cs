7a382b8e540de91c49226f38b17b579cd89a5f87
Adam <57573881+AdamCarter11@users.noreply.github.com>
2022-01-25 16:59:37 -0800

    trying stuff out

--- Harmonia/Assets/Scripts/SongConverter/FreqReader.cs
+++ Harmonia/Assets/Scripts/SongConverter/FreqReader.cs
@@ -23,11 +23,11 @@ public class FreqReader : MonoBehaviour
         {
             Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0), new Vector3(i, spectrum[i + 1] + 10, 0), Color.red);
             Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
-            print(spectrum[150]);
+            //print(spectrum[i]);
             Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green);
             Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.blue);
         }
-        
+        print(spectrum[50]);        
 
         /*
         float[] curSpectrum = new float[1024];
